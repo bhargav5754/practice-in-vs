87abf3b54026221e6ce03bd0d4238e58b0e037c5
bhargav5754 <bhargavkumarvemulaplli@gmail.com>
2026-02-25 09:21:22 +0530

    updated files

---
+++ hari.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Hello, World!"
+echo "This is a simple bash script."
+echo "You can add more commands here to perform various tasks."
+echo "For example, you can list files in the current directory:"
+ls -l
+echo "Or you can display the current date and time:"
+date
+echo "Feel free to modify this script to suit your needs!"
