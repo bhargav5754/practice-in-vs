699290ff79f4bb5460c804fbf6f3853d4bf2a494
bhargav5754 <bhargavkumarvemulaplli@gmail.com>
2026-02-25 09:27:05 +0530

    describe what you added

--- hari.sh
+++ hari.sh
@@ -7,3 +7,6 @@ ls -l
 echo "Or you can display the current date and time:"
 date
 echo "Feel free to modify this script to suit your needs!"
+    jkjjjdck
+echo "Script execution completed."      
+    
